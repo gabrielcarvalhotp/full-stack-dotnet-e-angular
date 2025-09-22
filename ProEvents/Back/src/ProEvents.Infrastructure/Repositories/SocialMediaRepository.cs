593c494d8dbaf3a8273ddd9cfa3d0fc24df9e546
Gabriel.Oliveira <gabrieltp2004@gmail.com>
2025-09-22 12:53:31 -0300

    SocialMedia repository impl

--- ProEvents/Back/src/ProEvents.Infrastructure/Repositories/SocialMediaRepository.cs
+++ ProEvents/Back/src/ProEvents.Infrastructure/Repositories/SocialMediaRepository.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProEvents.Domain.Entities;
+using ProEvents.Infrastructure.Abstractions;
+using ProEvents.Infrastructure.Context;
 
 namespace ProEvents.Infrastructure.Repositories
 {
-    public class SocialMediaRepository
+    public class SocialMediaRepository : RepositoryBase, ISocialMediaRepository
     {
+        public SocialMediaRepository(AppDbContext context) : base(context) { }
 
+        public async Task<IEnumerable<SocialMedia>> GetSocialMediasByEventIdAsync(int eventId, CancellationToken cancellationToken = default)
+        {
+            return await _context.SocialMedias.Where(sm => sm.EventId == eventId).ToListAsync(cancellationToken);
+        }
+        public async Task<IEnumerable<SocialMedia>> GetSocialMediasBySpeakerIdAsync(int speakerId, CancellationToken cancellationToken = default)
+        {
+            return await _context.SocialMedias.Where(sm => sm.SpeakerId == speakerId).ToListAsync(cancellationToken);
+        }
     }
 }
